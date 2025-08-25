0aa46c4fe365ab3ae57ac6e3e9f68bb037e74c57
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2025-08-25 17:50:21 +0300

    Impleminting Data Structures Using CSharp

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<int> ints = new List<int>() { 1, 2, 3, 4, 5 };
+
+
+        }
+
+        
+    }
+}
