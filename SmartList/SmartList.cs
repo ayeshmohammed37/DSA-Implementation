0c9307b7707af21f6be69b6f24690799ac07a576
ayeshmohammed37 <ayeshmohammed37@gmail.com>
2026-07-17 22:35:46 +0300

    SmartList: Fixs

--- SmartList/SmartList.cs
+++ SmartList/SmartList.cs
@@ -7,77 +7,67 @@ namespace DSA_Implementation.SmartList
     internal class SmartList<T>
     {
         private T[] _data;
-        private int _capacity;
         private int _count;
 
-        public int Capacity { get => _capacity; private set => _capacity = value; }
-        public int Count { get => _count; private set => _count = value; }
+        public int Capacity => _data.Length;
+        public int Count => _count;
 
         public SmartList()
         {
-            this._data = new T[4];
-            this.Capacity = 4;
-            this.Count = 0;
+            _data = new T[4];
+            _count = 0;
         }
 
         public SmartList(int capacity)
         {
-            this.Capacity = capacity;
-            this._data = new T[capacity];
-            this.Count = 0;
+            if (capacity <= 0)
+            {
+                throw new Exception("Capacity Can't be zero or negative.");
+            }
+
+            _data = new T[capacity];
+            _count = 0;
         }
 
         public T this[int index]
         {
             get 
-            { 
-                if (checkIndex(index))
-                {
-                    return _data[index];
-                }
-                else
+            {
+                if (index >= Count || index < 0)
                 {
                     throw new IndexOutOfRangeException("Index out of range");
                 }
+                
+                return _data[index];
             }
             set
             {
-                if (checkIndex(index))
-                {
-                    _data[index] = value;
-                }
-                else
+                if (index >= Count || index < 0)
                 {
                     throw new IndexOutOfRangeException("Index out of range");
                 }
+                
+                _data[index] = value;
             }
         }
 
         public void Add(T item)
         {
-            if (Count == Capacity)
+            if (_count == Capacity)
             {
                 extend();
             }
-            _data[Count++] = item;
+            _data[_count++] = item;
         }
 
         private void extend()
         {
-            Capacity = Capacity * 2;
+            int newCapacity = Capacity == 0? 4 : Capacity * 2;
             T[] _temp = new T[Capacity];
-            for (int i = 0; i < Count; i++)
-                _temp[i] = _data[i];
-            _data = _temp;
-        }
 
-        private bool checkIndex(int idx)
-        {
-            if (idx >= Count || idx < 0)
-            {
-                return false;
-            }
-            return true;
+            Array.Copy(_data, _temp, _count);
+            
+            _data = _temp;
         }
 
     }
